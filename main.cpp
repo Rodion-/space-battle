0417de0b0ce2c0ce315653d5b59bcaf259e07ca0
rodion <rodion.carpediem@mail.ru>
2022-09-21 18:53:22 +0300

    add: main

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+#include <math.h>
+
+
+
+int main ( int argc , char** argv )
+{
+	{
+		using namespace std;
+		
+        	try
+        	{
+            	cout<<"space battle"<<endl;
+
+
+
+        	}
+        	catch( double err )
+        	{
+          	  cout<<"error : " << err <<endl;
+       	 }
+	}
+
+	return 0;
+}
