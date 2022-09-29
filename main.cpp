567fe437c27745e5e90f43e8e86e9c42c1e5bbb9
rodion <rodion.carpediem@mail.ru>
2022-09-29 18:03:40 +0300

    add : googletests

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include <memory>
 #include <assert.h>
 
+#include <gtest/gtest.h>
+
+
 typedef struct position
 {
     int x;
@@ -146,118 +149,113 @@ MoveAdapter move_spaceship ( std::move(spaceship) );
 RotationAdapter rotate_spaceship ( std::move(spaceship) );
 
 
+//  Для объекта, находящегося в точке (12, 5) и движущегося со скоростью (-7, 3) движение меняет положение объекта на (5, 8)
+
+TEST( test1, test)
+{
+    position spaceship_position{ 12 , 5 };
+    velocity spaceship_velocity{ -7 , 3 };
+
+    move_spaceship.set_position( &spaceship_position );
+    move_spaceship.set_velocity( &spaceship_velocity );
+
+    move_spaceship.execute();
+
+    move_spaceship.get_position( &spaceship_position );
+
+    EXPECT_EQ( spaceship_position.x , 5 );
+    EXPECT_EQ( spaceship_position.y , 8 );
+
+}
+
+//  Попытка сдвинуть объект, у которого невозможно прочитать положение в пространстве, приводит к ошибке
+
+TEST( test2 , test )
+{
+    using namespace std;
+
+    position spaceship_position{ 0 , 0 };
+    move_spaceship.set_position( &spaceship_position );
+
+    EXPECT_ANY_THROW ( move_spaceship.execute(); );
+}
+
+//  Попытка сдвинуть объект, у которого невозможно прочитать значение мгновенной скорости, приводит к ошибке
+
+TEST( test3 , test )
+{
+    using namespace std;
+
+    velocity spaceship_velocity{ 0 , 0 };
+    move_spaceship.set_velocity( &spaceship_velocity );
+
+    EXPECT_ANY_THROW ( move_spaceship.execute(); );
+}
+
+//  Попытка сдвинуть объект, у которого невозможно изменить положение в пространстве, приводит к ошибке
+
+TEST( test4 , test )
+{
+    using namespace std;
+
+    position spaceship_position{ 0 , 0 };
+    velocity spaceship_velocity{ 0 , 0 };
+
+    move_spaceship.set_velocity( &spaceship_velocity );
+    move_spaceship.set_position( &spaceship_position );
+
+    EXPECT_ANY_THROW ( move_spaceship.execute(); );
+}
+
+//  Поворот на 90 градусов
+
+TEST( test5 , test )
+{
+    using namespace std;
+
+    int angle = 0;
+
+    rotate_spaceship.set_angle( 90 );
+
+    rotate_spaceship.execute();
+
+    rotate_spaceship.get_angle( &angle );
+
+    EXPECT_EQ ( angle , 90 );
+}
+
+//  Попытка сдвинуть объект, у которого невозможно изменить положение в пространстве, приводит к ошибке
+
+TEST( test6 , test )
+{
+    using namespace std;
+
+    rotate_spaceship.set_angle( 0 );
+
+    EXPECT_ANY_THROW (  rotate_spaceship.execute(); );
+}
+
 
 int main ( int argc , char** argv )
 {
-	{
-		using namespace std;
+    #if G_TEST_RUN
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+    #endif
+
+    using namespace std;
 		
-        	try
-        	{
-            	cout<<"space battle"<<endl;
+    try
+    {
+        cout<<"space battle"<<endl;
 
-                //  Для объекта, находящегося в точке (12, 5) и движущегося со скоростью (-7, 3) движение меняет положение объекта на (5, 8)
 
-                {
-                    position spaceship_position{ 12 , 5 };
-                    velocity spaceship_velocity{ -7 , 3 };
+    }
+    catch( ... )
+    {
+      cout<<"error : " <<endl;
 
-                    move_spaceship.set_position( &spaceship_position );
-                    move_spaceship.set_velocity( &spaceship_velocity );
+    }
 
-                    move_spaceship.execute();
-
-                    move_spaceship.get_position( &spaceship_position );
-
-                    assert( spaceship_position.x == 5 );
-                    assert( spaceship_position.y == 8 );
-                }
-
-                //  Попытка сдвинуть объект, у которого невозможно прочитать положение в пространстве, приводит к ошибке
-                {
-                    try
-                    {
-                        position spaceship_position{ 0 , 0 };
-
-                        move_spaceship.set_position( &spaceship_position );
-
-                        move_spaceship.execute();
-                    }
-                    catch ( int error )
-                    {
-                        cout << "unmovable object !" << endl;
-                    }
-                }
-                //  Попытка сдвинуть объект, у которого невозможно прочитать значение мгновенной скорости, приводит к ошибке
-                {
-                    try
-                    {
-                        velocity spaceship_velocity{ 0 , 0 };
-
-                        move_spaceship.set_velocity( &spaceship_velocity );
-
-                        move_spaceship.execute();
-                    }
-                    catch ( int error )
-                    {
-                        cout << "unmovable object !" << endl;
-                    }
-                }
-                //  Попытка сдвинуть объект, у которого невозможно изменить положение в пространстве, приводит к ошибке
-                {
-                    try
-                    {
-                        position spaceship_position{ 0 , 0 };
-                        velocity spaceship_velocity{ 0 , 0 };
-
-                        move_spaceship.set_velocity( &spaceship_velocity );
-                        move_spaceship.set_position( &spaceship_position );
-
-                        move_spaceship.execute();
-                    }
-                    catch ( int error )
-                    {
-                        cout << "unmovable object !" << endl;
-                    }
-                }
-                //  Поворот на 90 градусов
-                {
-                    try
-                    {
-                        int angle = 0;
-
-                        rotate_spaceship.set_angle( 90 );
-
-                        rotate_spaceship.execute();
-
-                        rotate_spaceship.get_angle( &angle );
-
-                        assert( angle == 90 );
-                    }
-                    catch ( int error )
-                    {
-                        cout << "unmovable object !" << endl;
-                    }
-                }
-                 //  Попытка сдвинуть объект, у которого невозможно изменить положение в пространстве, приводит к ошибке
-                {
-                    try
-                    {
-                        rotate_spaceship.set_angle( 0 );
-
-                        rotate_spaceship.execute();
-                    }
-                    catch ( int error )
-                    {
-                        cout << "unmovable object !" << endl;
-                    }
-                }
-        	}
-            catch( ... )
-        	{
-              cout<<"error : " <<endl;
-       	 }
-	}
-
-	return 0;
+    return 0;
 }
